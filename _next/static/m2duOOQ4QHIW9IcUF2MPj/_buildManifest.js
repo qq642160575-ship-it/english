c7e5d6e48820ac5362f9a69d3fcff6ebed78d70f
qq642160575-ship-it <qq642160575-ship-it@users.noreply.github.com>
2026-04-29 08:43:33 +0000

    deploy: 634bac3b593b0c9c4ef94d8833b53e0a8ab99a95

--- _next/static/m2duOOQ4QHIW9IcUF2MPj/_buildManifest.js
+++ _next/static/m2duOOQ4QHIW9IcUF2MPj/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/english//_next/:path+",
-        "destination": "/english/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
